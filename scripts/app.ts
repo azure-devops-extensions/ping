98f8dd57da009a2888ff0cc71d238befe15e6d8b
Otto Streifel <ottost@microsoft.com>
2017-03-28 21:48:08 -0700

    v0.1.3 Update logo

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,7 +1,7 @@
-import { MessageHelper } from "./logic/messageHelper";
 
 var actionProvider =  {
     getMenuItems: (context) => {
+        console.log("context", context);
         return [<IContributedMenuItem>{
             title: "Work Item Menu Action",
             action: (actionContext) => {
@@ -9,10 +9,6 @@ var actionProvider =  {
                     || (actionContext.ids && actionContext.ids.length > 0 && actionContext.ids[0])
                     || (actionContext.workItemIds && actionContext.workItemIds.length > 0 && actionContext.workItemIds[0]);
                     
-                if (workItemId) {
-                    let messageHelper = new MessageHelper();
-                    alert(messageHelper.format([workItemId]));
-                }
             }
         }];
     }
